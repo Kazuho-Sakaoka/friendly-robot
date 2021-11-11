e22f176e63990b734bb243a753376ec9d93df886
坂岡一穂 <kazuho-sakaoka@kazuhosakaoka.onmicrosoft.com>
2021-11-11 22:57:56 +0900

    ML チュートリアル　センチメント分析

---
+++ SentimentAnalysis/SentimentAnalysis/Program.cs
@@ -0,0 +1,100 @@
+﻿using SentimentAnalysis;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using static Microsoft.ML.DataOperationsCatalog;
+
+
+static TrainTestData LoadData(MLContext mlContext, string dataPath)
+{
+    var dataView = mlContext.Data.LoadFromTextFile<SentimentData>(dataPath, hasHeader: false);
+    var splitDataView = mlContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
+    return splitDataView;
+}
+
+static ITransformer BuildAndTrainModel(MLContext mlContext, IDataView splitTrainSet)
+{
+    var estimator = mlContext.Transforms.Text.FeaturizeText(outputColumnName: "Features", inputColumnName: nameof(SentimentData.SentimentText))
+        .Append(mlContext.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label", featureColumnName: "Features"));
+    Console.WriteLine("=============== Create and Train the Model ===============");
+    var model = estimator.Fit(splitTrainSet);
+    Console.WriteLine("=============== End of training ===============");
+    Console.WriteLine();
+    return model;
+}
+
+static void Evaluate(MLContext mlContext, ITransformer model, IDataView splitTestSet)
+{
+    Console.WriteLine("=============== Evaluating Model accuracy with Test data===============");
+    var predictions = model.Transform(splitTestSet);
+    var metrics = mlContext.BinaryClassification.Evaluate(predictions, "Label");
+
+    Console.WriteLine();
+    Console.WriteLine("Model quality metrics evaluation");
+    Console.WriteLine("--------------------------------");
+    Console.WriteLine($"Accuracy: {metrics.Accuracy:P2}");
+    Console.WriteLine($"Auc: {metrics.AreaUnderRocCurve:P2}");
+    Console.WriteLine($"F1Score: {metrics.F1Score:P2}");
+    Console.WriteLine("=============== End of model evaluation ===============");
+}
+
+static void UseModelWithSingleItem(MLContext mlContext, ITransformer model)
+{
+    var predictionFunction = mlContext.Model.CreatePredictionEngine<SentimentData, SentimentPrediction>(model);
+    var sampleStatement = new SentimentData
+    {
+        SentimentText = "This was a very bad steak"
+    };
+    var resultPrediction = predictionFunction.Predict(sampleStatement);
+
+    Console.WriteLine();
+    Console.WriteLine("=============== Prediction Test of model with a single sample and test dataset ===============");
+
+    Console.WriteLine();
+    Console.WriteLine($"Sentiment: {resultPrediction.SentimentText} | Prediction: {(Convert.ToBoolean(resultPrediction.Prediction) ? "Positive" : "Negative")} | Probability: {resultPrediction.Probability} ");
+
+    Console.WriteLine("=============== End of Predictions ===============");
+    Console.WriteLine();
+}
+
+static void UseModelWithBatchItems(MLContext mlContext, ITransformer model)
+{
+    var sentiments = new[]
+    {
+        new SentimentData
+        {
+            SentimentText = "This was a horrible meal"
+        },
+        new SentimentData
+        {
+            SentimentText = "I love this spaghetti."
+        }
+    };
+
+    var batchComments = mlContext.Data.LoadFromEnumerable(sentiments);
+
+    var predictions = model.Transform(batchComments);
+
+    // Use model to predict whether comment data is Positive (1) or Negative (0).
+    var predictedResults = mlContext.Data.CreateEnumerable<SentimentPrediction>(predictions, reuseRowObject: false);
+
+    Console.WriteLine();
+
+    Console.WriteLine("=============== Prediction Test of loaded model with multiple samples ===============");
+
+    foreach (SentimentPrediction prediction in predictedResults)
+    {
+        Console.WriteLine($"Sentiment: {prediction.SentimentText} | Prediction: {(Convert.ToBoolean(prediction.Prediction) ? "Positive" : "Negative")} | Probability: {prediction.Probability} ");
+    }
+    Console.WriteLine("=============== End of predictions ===============");
+}
+
+var dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "yelp_labelled.txt");
+var mlContext = new MLContext();
+var splitDataView = LoadData(mlContext, dataPath);
+
+
+var model = BuildAndTrainModel(mlContext, splitDataView.TrainSet);
+Evaluate(mlContext, model, splitDataView.TestSet);
+
+UseModelWithSingleItem(mlContext, model);
+UseModelWithBatchItems(mlContext, model);
